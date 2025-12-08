4d5ba30e4d47d549c36bd201b0898ce8b6f58e2b
Diksha <diksha@macbookpro>
2025-12-08 15:43:35 -0800

    Extra Milestone 1

--- main.cpp
+++ main.cpp
@@ -18,10 +18,15 @@ int main(){
 	}
 	string movie;
 	string rating;
-	while (fin >> movie >> rating)){
-		int r = stoi(rating)
+	while (fin >> movie >> rating){
+		int r = stoi(rating);
 		ratings[movie].push_back(r);
 	}
-	for (auto it = ratings.begin(); it != ratings.end(); it++)
-	
+	cout << "Movie   \t\tRatings" << endl;
+	for (auto it = ratings.begin(); it != ratings.end(); it++){
+		cout << "Movie: " << it->first << "\n\tRatings:  ";
+		for (const int &i : it->second)
+			cout << i << "  ";
+		cout << endl << endl;
+	}
 }
