38566266dc973e5f84927cd3784e6b87476ab572
Diksha <diksha@macbookpro>
2025-12-08 15:22:08 -0800

    implementing map

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 // COMSC-210 | Extra Credit 2 | Diksha Tara Natesan
 // IDE used: Vim/Terminal
 
-
 #include <iostream>
 #include <fstream>
 #include <map>
@@ -10,9 +9,15 @@
 using namespace std;
 
 int main(){
+	map<string, vector> ratings;
 	
-
-
-
+	ifstream fin("movies.txt");
+	if (!fin) {
+		cout << "Error: could not open bakerydata.txt" << endl;
+		return 1;
+	}
+	string line;
+	while (getline(fin, line))
+		ratings[line].push_back();
 
 }
